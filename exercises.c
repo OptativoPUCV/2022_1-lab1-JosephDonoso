89e6d1f3e1bc653c224363660c1f071936aaf392
Your Name <you@example.com>
2022-03-10 15:43:23 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -12,21 +12,20 @@ y en c almacena el valor de la suma de a más b.
 */
 
 void suma(int a, int b, int * c) {
-   (*c) = a + b;
+   *c = a + b;
 }
 
 /*
 Esta función suma los N primeros números del arreglo a
 */
 int sumaN(int a[], int N) {
-    int i;
-    int tot = 0;
+    int total = 0;
 
-    for(i = 0; i < N; i++) {
-      tot += a[i];
+    for(int i = 0; i < N; i++) {
+      total += a[i];
     }
 
-    return tot;
+    return total;
 }
 
 /*
@@ -37,7 +36,7 @@ Utilice la función sumaN.
 */
 
 void sumaNultimos(int a[], int n, int m, int * suma) {
-
+   *suma = sumaN( (a + (n-m) ), m);
 }
 
 /*
